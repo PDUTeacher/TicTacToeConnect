272f47e0178d04b0c5188ea45314db7db7c438a4
Alex9003 <agl.luchko@gmail.com>
2026-03-07 14:01:10 +0200

    make mistake

---
+++ app/src/main/java/com/example/tic_tac_connect/SettingsActivity.java
@@ -0,0 +1,17 @@
+package com.example.tic_tac_connect;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+
+
+    }
+}
