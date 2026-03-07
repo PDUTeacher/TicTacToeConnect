30971bc3c43eed67967fee6a7abdf077aebd4af5
Alex9003 <agl.luchko@gmail.com>
2026-03-07 14:14:50 +0200

    add java lass

--- app/src/main/java/com/example/tic_tac_connect/SettingsActivity.java
+++ app/src/main/java/com/example/tic_tac_connect/SettingsActivity.java
@@ -5,12 +5,30 @@ import android.os.Bundle;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.switchmaterial.SwitchMaterial;
+import com.google.android.material.textfield.TextInputEditText;
+
 public class SettingsActivity extends AppCompatActivity {
+
+    public static final String PREFS_NAME ="";
+    public static final String KEY_PLAYER_NAME ="";
+    public static final String KEY_GAME_STYLE="";
+    public static final String KEY_DIFFICULT  ="";
+    public static final String KEY_VOLUME ="";
+    public static final String KEY_VOLUME ="";
+
+
+    private TextInputEditText setPlayName;
+    private MaterialButtonToggleGroup toggleStyle;
+    private Slider sliderDifficulty;
+    private Slider sliderVolume;
+    private SwitchMaterial switchVibration;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
 
 
     }
